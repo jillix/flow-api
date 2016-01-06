5dcd24ac7efe3682e8ae7ea4bb4cd57f65e6d2bb
Adrian Ottiker <adrian@ottiker.com>
2016-01-06 12:59:45 +0000

    remove test index file

--- index.js
+++
@@ -1,9 +0,0 @@
-// return cached api instance
-exports.cache = function (name) {
-    // ..get chached comp
-};
-
-// return api instance
-exports.factory = function (name) {
-    // ..composition instance for every app
-};
