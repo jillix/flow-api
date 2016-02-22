45f3459abfedd6a57acfe1798a5b3a1c64db789d
Ionică Bizău <bizauionica@gmail.com>
2016-02-22 12:06:41 +0200

    Search packages using fuzzy search

--- example/index.js
+++ example/index.js
@@ -7,3 +7,16 @@ fa.readInstance("foo", (err, data) => {
     console.log(err, data);
     /* do something with the "foo" instance */
 });
+
+FlowApi.searchModules("html", function (err, data) {
+    console.log(err || data);
+});
+// [ { string: 'view::Render HTML templates',
+//     score: 26,
+//     index: 2,
+//     original:
+//      { name: 'view',
+//        version: '0.1.0',
+//        description: 'Render HTML templates',
+//        ...
+//        _id: 'view@0.1.0' } } ]
