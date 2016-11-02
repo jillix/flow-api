acee5bc0457f6b32663514508b6f1f062a4629ed
Dan Andrei <andreidani96@gmail.com>
2016-11-02 15:26:33 +0200

    Code refactor.

--- index.js
+++ index.js
@@ -1,59 +1,30 @@
-'use strict'
-
-const flowAdapter = require('./flow_adapter');
+'use strict';
 
 // Dependencies
-var SetOrGet = require('set-or-get')
-  , IterateObject = require('iterate-object')
-  , Handlers = require('./lib');
+const adapter = require('./lib/adapter');
+const modules = require('./lib/modules');
+const events= require('./lib/events');
 
-module.exports = {};
+// export API methods
+module.exports = {
+    vis: {
+        modules: modules.modules,
+        instances: modules.instances,
+        instance: modules.instance,
+        event: events.event,
+        handler: events.handler
+    },
+    adapter: adapter
+};
+
 
 /**
  * Main init function
  *
  * @private
 */
-module.exports.init = function (config, ready) {
+module.exports.init = (config, ready) => {
     ready();
-}
+};
 
-module.exports.adapter = flowAdapter;
 
-/**
- * Generate a wrapper for the appended data handler
- *
- * @private
-*/
-function generateMethod (prefix, handler) {
-    return function () {
-
-        // get function arguments
-        var args = arguments;
-
-        /* Do custom stuff here before the handler is called
-         * ...
-        */
-
-        // check if 
-        handler.apply(this, args);
-    }
-}
-
-/**
- * Append the available data handlers to the module.exports object
- *
- * @private
-*/
-function appendDataHandlers (object, parent, prefix) {
-    IterateObject(object, function (handler, name) {
-        var cPref = prefix ? prefix + '.' + name : name;
-        if (typeof handler === 'object') {
-            return appendDataHandlers(handler, SetOrGet(parent, name, {}), cPref);
-        }
-        parent[name] = generateMethod(cPref, handler);
-    });
-}
-
-// start appending the data handlers
-appendDataHandlers(Handlers, module.exports, '');
