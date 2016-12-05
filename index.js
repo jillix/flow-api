20e331aec9f31b545bcd280e1f4f74edd1b2d884
Dan Andrei <andreidani96@gmail.com>
2016-12-05 13:55:36 +0200

    Refactor. Added detail apis.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // Dependencies
 const adapter = require('./lib/adapter');
 const modules = require('./lib/modules');
+const instances = require('./lib/instances');
 const events = require('./lib/events');
 const cayley = require('./lib/cayley');
 const utils = require('./lib/utils');
@@ -11,11 +12,23 @@ const utils = require('./lib/utils');
 module.exports = {
     connect: cayley.connect,
     vis: {
-        modules: modules.modules,
-        instances: modules.instances,
-        instance: modules.instance,
-        event: events.event,
-        handler: events.handler
+        modules: {
+            get: modules.get,
+            details: modules.details
+        },
+        instances: {
+            get: instances.get,
+            create: instances.create,
+            getOne: instances.getOne,
+            details: instances.details
+        },
+        events: {
+            getOne: events.getOne,
+            create: events.create,
+            details: events.details,
+            handler: events.handler,
+            handlerDetails: events.handlerDetails
+        }
     },
     adapter: adapter,
     utils: utils
