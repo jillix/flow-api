1986fe12b9400da03e2c52c051b63bc8063052dc
Adrian Ottiker <adrian@ottiker.com>
2016-12-13 17:13:07 +0100

    export sequence methods

--- index.js
+++ index.js
@@ -3,28 +3,15 @@
 //const instances = require('./lib/instances');
 //const events = require('./lib/events');
 const cayley = require('./lib/cayley');
+const sequence = require('./lib/sequence');
 const utils = require('./lib/utils');
 
 // export API methods
 module.exports = {
     connect: cayley.connect,
     get: {
-        flow: (scope, state, args, data, next) => {
-
-            if (!data.id) {
-                return next(new Error('Flow-api.get.flow: missing sequence id.'));
-            }
-
-            let role = !data.session || !data.session.role ? scope.env.role : data.session.role;
-
-            data.readable = cayley.flow(state.g, data.id, role);
-            data.readable[0].pause();
-            data.readable[1].pause();
-            data.readable[2].pause();
-            data.resume = data.readable;
-            next(null, data);
-        }
-        //entrypoints: sequence.entrypoints,
+        flow: sequence.flow,
+        entrypoints: sequence.entrypoints,
         //sequence: sequence.detail,
         //handler: sequence.handler,
         //method: method
