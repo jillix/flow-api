e4a3d4fe5467f0a1e89123f744eee5e3aa9d2553
Adrian Ottiker <adrian@ottiker.com>
2017-01-14 22:26:16 +0100

    create store instance, stream result for post requests

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-// Dependencies
-//const modules = require('./lib/modules');
-//const instances = require('./lib/instances');
-//const events = require('./lib/events');
-const cayley = require('cayley');
-const Adapter = require('./lib/cayley_adapter');
-const VIS = require('./lib/cayley_vis');
-const GET = require('./lib/cayley_get');
-const POST = require('./lib/cayley_post');
-const DELETE = require('./lib/cayley_delete');
-const utils = require('./lib/utils');
+"use strict"
 
-function connect (state, db) {
-    state.client = cayley(db);
-    state.g = state.client.graph;
-}
+const FLOW = require('./lib/request_flow');
+const VIS = require('./lib/request_vis');
+const GET = require('./lib/request_get');
+const POST = require('./lib/request_post');
+const DELETE = require('./lib/request_delete');
+const Store = require('./lib/store');
+const utils = require('./lib/utils');
 
 function getHandler (Methods, fn, params) {
 
@@ -23,12 +16,13 @@ function getHandler (Methods, fn, params) {
 
     return (scope, state, args, data, next) => { 
 
-        // create db client for state
-        if (!state.client) {
-            connect(state, scope.env.db);
+        // create store instance on state
+        if (!state.store) {
+            // TODO handle errors
+            state.store = Store(scope.env.FlowApiStore);
         }
 
-        let query_args = [state.g];
+        let query_args = [state.store];
         for (let i = 0, l = params.length; i < l; ++i) {
 
             if (typeof data[params[i]] === 'undefined') {
@@ -41,18 +35,9 @@ function getHandler (Methods, fn, params) {
         query_args.push(!data.session || !data.session.role ? scope.env.role : data.session.role);
 
         data.readable = Methods[fn].apply(null, query_args);
-
-        try {
-            if (data.readable instanceof Array) {
-                data.readable.forEach(stream => stream.pause());
-            } else {
-                data.readable.pause();
-            }
-        } catch (err) {
-            return next(err);
-        }
-
+        data.readable.pause();
         data.resume = data.readable;
+
         next(null, data);
     }
 } 
@@ -60,80 +45,27 @@ function getHandler (Methods, fn, params) {
 function postHandler (METHOD) {
     return (scope, state, args, data, next) => {
 
-        // create db client for state
-        if (!state.client) {
-            connect(state, scope.env.db);
+        // create store instance on state
+        if (!state.store) {
+            state.store = Store(scope.env.FlowApiStore);
         }
 
-        let count = 1;
-        const errors = [];
-        const success = [];
-        const done = () => {
-            data.body = {
-                success: success,
-                errors: errors
-            };
-
-            next(null, data);
-        };
-
-        // parse request
-        data.req.on('data', req => {
-            ++count
-            METHOD(state.client, req, (err, res) => {
-
-                if (err) {
-
-                    if (err instanceof Error) {
-                        err = err.message;
-                    }
-
-                    errors.push(err);
-                } else {
-                    success.push(res);
-                }
-
-                if (--count === 0) {
-                    done();
-                }
-            });
-        });
-
-        data.req.on('error', (err) => next(err));
-
-        // parse end
-        data.req.on('end', () => {
-            if (--count === 0) {
-                done();
-            } 
-        });
+        data.readable = req.pipe(METHOD(state.store));
+        next(null, data);
     };
-}
-
-function deleteHandler (scope, state, args, data, next) {
-
-    // create db client for state
-    if (!state.client) {
-        connect(state, scope.env.db);
-    }
-
-    DELETE(state.client, data.req.url);
-    data.body = {status: "ok"};
-    next(null, data);
 }
 
 // export API methods
 module.exports = {
-    _connect: connect,
     utils: utils,
-    flow: getHandler(Adapter, 'flow', ['id']),
+    flow: getHandler(FLOW, 'flow', ['id']),
     vis: {
-        networks: getHandler(VIS, 'vis_networks', ['id']),
-        entrypoints: getHandler(VIS, 'vis_entrypoints', ['id']),
-        entrypoint: getHandler(VIS, 'vis_entrypoint', ['id']),
-        sequence: getHandler(VIS, 'vis_sequence', ['id']),
-        handler: getHandler(VIS, 'vis_handler', ['id']),
-        object: getHandler(VIS, 'vis_object', ['id'])
+        networks: getHandler(VIS, 'networks', ['id']),
+        entrypoints: getHandler(VIS, 'outNodeNames', ['id']),
+        entrypoint: getHandler(VIS, 'outNodeNames', ['id']),
+        sequence: getHandler(VIS, 'outNodeNames', ['id']),
+        handler: getHandler(VIS, 'handler', ['id']),
+        object: getHandler(VIS, 'getObject', ['id'])
     },
     get: getHandler(GET, 'get', ['id', 'type']),
     save: postHandler(POST),
